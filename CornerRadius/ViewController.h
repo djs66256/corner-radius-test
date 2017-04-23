60c5486c472ae0c5a915fbe69a2bc636a32dfd5a
Daniel <djs66256@163.com>
2017-04-23 22:29:11 +0800

    Initial Commit

---
+++ CornerRadius/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CornerRadius
+//
+//  Created by daniel on 2017/4/23.
+//  Copyright © 2017年 Daniel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CornerRadius/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  CornerRadius
+//
+//  Created by daniel on 2017/4/23.
+//  Copyright © 2017年 Daniel. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
